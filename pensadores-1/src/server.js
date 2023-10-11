419503ff255750b3dc20c0e552c5d24981db88e6
Vitor Samuel <108810741+VitorSmelo021@users.noreply.github.com>
2023-10-11 08:22:54 -0300

    Add files via upload

--- pensadores-1/src/server.js
+++ pensadores-1/src/server.js
@@ -1,11 +1,21 @@
 const express = require("express")
 const router = require("./routes")
+const exphbs = require("express-handlebars");
+const { response } = require("express");
 
 require("./database/index");
 
 const app = express();
 app.use(express.json());
+
+// Importando engine handlebars
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
 app.use(router);
 
+app.get('/main', (response,request) => {
+    return response.prependListener('main')
+})
 
 app.listen(3333, console.log("Servidor ta pegando na porta 3333 moral😎👍"))
